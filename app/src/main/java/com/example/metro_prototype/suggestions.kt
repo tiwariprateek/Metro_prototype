5c39a933eae944b80c43e0a2da666b4fbc0b8766
Prateek Tiwari <prateektiwari5399@gmail.com>
2019-07-24 00:46:36 +0530

    Listview suggestions debug

--- app/src/main/java/com/example/metro_prototype/suggestions.kt
+++ app/src/main/java/com/example/metro_prototype/suggestions.kt
@@ -9,54 +9,20 @@ import android.view.View
 import android.widget.*
 
 class suggestions : AppCompatActivity() {
-    val stations= arrayOf("Noida City Centre","Noida Golf Course",
-        "Botanical Garden",
-        "Noida Sector 18",
-        "Noida Sector 16",
-        "Noida Sector 15",
-        "New Ashok Nagar",
-        "Mayur Vihar Extension",
-        "Mayur Vihar - I",
-        "Akshardham",
-        "Yamuna Bank",
-        "Indraprastha",
-        "Pragati Maidan",
-        "Mandi House",
-        "Barakhambha Road",
-        "Rajiv Chowk",
-        "Ramakrishna Ashram Marg",
-        "Jhandewalan",
-        "Karol Bagh",
-        "Rajendra Place",
-        "Patel Nagar",
-        "Shadipur",
-        "Kirti Nagar",
-        "Moti Nagar",
-        "Ramesh Nagar",
-        "Rajouri Garden",
-        "Tagore Garden",
-        "Subhash Nagar",
-        "Tilak Nagar",
-        "Janakpuri East",
-        "Janakpuri West",
-        "Uttam Nagar East",
-        "Uttam Nagar West",
-        "Nawada",
-        "Dwarka Morh",
-        "Dwarka",
-        "Dwarka Sector 14",
-        "Dwarka Sector 13",
-        "Dwarka Sector 12",
-        "Dwarka Sector 11",
-        "Dwarka Sector 10",
-        "Dwarka Sector 9",
-        "Dwarka Sector 8","Dwarka Sector 21")
+    val stations= arrayOf("Noida City Centre","Noida Golf Course","Botanical Garden","Noida Sector 18","Noida Sector 16",
+        "Noida Sector 15","New Ashok Nagar", "Mayur Vihar Extension","Mayur Vihar - I", "Akshardham", "Yamuna Bank",
+        "Indraprastha", "Pragati Maidan", "Mandi House", "Barakhambha Road", "Rajiv Chowk", "Ramakrishna Ashram Marg",
+        "Jhandewalan", "Karol Bagh", "Rajendra Place", "Patel Nagar", "Shadipur", "Kirti Nagar", "Moti Nagar",
+        "Ramesh Nagar", "Rajouri Garden", "Tagore Garden", "Subhash Nagar", "Tilak Nagar", "Janakpuri East", "Janakpuri West",
+        "Uttam Nagar East", "Uttam Nagar West", "Nawada", "Dwarka Morh", "Dwarka", "Dwarka Sector 14", "Dwarka Sector 13",
+        "Dwarka Sector 12", "Dwarka Sector 11", "Dwarka Sector 10", "Dwarka Sector 9", "Dwarka Sector 8","Dwarka Sector 21")
     lateinit var suggestionslist:ListView
     lateinit var source:EditText
     lateinit var destination:EditText
     lateinit var adapter:ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         stations.sort()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suggestions)
@@ -66,6 +32,8 @@ class suggestions : AppCompatActivity() {
         adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stations)
         suggestionslist.adapter = adapter
         Log.d("Sorted", "Sorted list $")
+        var src=false
+
         source.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
@@ -82,29 +50,40 @@ class suggestions : AppCompatActivity() {
         })
         suggestionslist.setOnItemClickListener { parent, view, position, id ->
             val element = adapter.getItem(position)
-            source.setText(element)
+            if (src==false){
+                source.setText(element)
+                src=true}
+            else
+                destination.setText(element)
+                src=false
+//            source.setText(element)
             Log.d("Prototype","v2 The source and destination is $source $destination")
             if (suggestionslist.visibility==View.VISIBLE) {
                 suggestionslist.visibility = View.INVISIBLE
             }
         }
-//        destination.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(s: Editable?) {
-//
-//            }
-//
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                (this@suggestions).adapter.filter.filter(s)
-//                //suggestionslist.visibility=View.VISIBLE
-//            }
-//        })
+        destination.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                (this@suggestions).adapter.filter.filter(s)
+                suggestionslist.visibility=View.VISIBLE
+            }
+        })
 //        suggestionslist.setOnItemClickListener { parent, view, position, id ->
 //            val element2 = adapter.getItem(position)
-//            destination.setText(element2)
+//            if (src==false){
+//                destination.setText(element2)
+//                src=true}
+//            else
+//                source.setText(element2)
 //            Log.d("Prototype","The source and destination is $source $destination")
 //            if (suggestionslist.visibility==View.VISIBLE) {
 //                suggestionslist.visibility = View.INVISIBLE
